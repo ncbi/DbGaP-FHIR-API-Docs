92dd2b5439aad095f69ea8a81ffbb305eed5d3bd
Bloom <bloomjm@ncbi.nlm.nih.gov>
2025-03-17 14:21:02 -0400

    add test_resolve_pages
    FHIR-2670

--- jupyter/test_dbgap_fhir.py
+++ jupyter/test_dbgap_fhir.py
@@ -15,3 +15,43 @@ def test_run_query_with_limit(MockSession):
 
     assert len(result) == 1
     assert result[0]["id"] == "1"
+
+
+@patch('requests.Session')
+def test_resolve_pages(MockSession):
+    mock_session = MockSession.return_value
+    mock_response_metadata = MagicMock()
+    mock_response_page_1 = MagicMock()
+    mock_response_page_2 = MagicMock()
+
+    mock_response_metadata.json.return_value = {
+        "metadata": "data",
+    }
+
+    # Mock the first page response
+    mock_response_page_1.json.return_value = {
+        "link": [{"relation": "next", "url": "http://example.com/page2"}],
+        "entry": [{"resource": {"id": "1"}}]
+    }
+
+    # Mock the second page response
+    mock_response_page_2.json.return_value = {
+        "entry": [{"resource": {"id": "2"}}]
+    }
+
+    # Set the return values for the session's get method
+    mock_session.get.side_effect = [mock_response_metadata,
+                                    mock_response_page_1,
+                                    mock_response_page_2]
+
+    client = DbGapFHIR(fhir_server="http://example.com")
+    bundle = {
+        "link": [{"relation": "next", "url": "http://example.com/page1"}],
+        "entry": [{"resource": {"id": "0"}}]
+    }
+
+    result = client.resolve_pages(bundle)
+
+    assert len(result) == 2
+    assert result[0]["entry"][0]["resource"]["id"] == "0"
+    assert result[1]["entry"][0]["resource"]["id"] == "1"
